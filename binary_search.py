522422aba327aa4e69e3ea2c15f4e2d1aae369da
OverLeo007 <sokolov_lev2003@mail.ru>
2022-10-17 19:41:11 +0700

    Fin 100% workable

--- binary_search.py
+++ binary_search.py
@@ -1,7 +1,20 @@
-"""Шаблон бинарного поиска"""
+"""
+Модуль содержащий функцию бинарного поиска,
+применимую к array из модуля my_array
+"""
+
+from __future__ import annotations
+
+from cython_code.my_array import array
 
 
-def search(sequence, item):
+def search(sequence: array, item: object) -> object | None:
+    """
+    Бинарный поиск в массиве
+    :param sequence: массив
+    :param item: элемент
+    :return: индекс элемента или None при его отсутствии
+    """
     ind = 0
     length = len(sequence) - 1
     if len(sequence) == 0:
